d3de5b1f2462574158df4003d9b4162db680d20c
Felipe da Rocha Ferreira <felipe179971@hotmail.com>
2021-05-23 22:06:28 -0300

    Começei a documentação (preciso fazer checagens)

--- R/Graphic.R
+++ R/Graphic.R
@@ -11,11 +11,10 @@ function(R){
   RR<-R%>%
     ggplot(aes(Treatment=Treatment,y=Ordem, x=`Mean`,color=Group)) +
     geom_point() +
-    scale_y_continuous(breaks=c(R$Ordem),label=c(as.character(R$Treatment)))+
+    scale_y_continuous(breaks=c(R$Ordem),labels=c(as.character(R$Treatment)),name="")+
     geom_errorbar(aes(xmin=min,xmax=max),width = 0.2,size  = 0.7) +
     theme_bw()+
-    labs(title = "Scott-Knott") +
-    theme(plot.title = element_text(size = 9, colour = "#7f7f7f"))
+    labs(title = "Scott-Knott")
 
   Grafico<-ggplotly(RR,tooltip = c("Treatment","x","Group"))%>%
     layout(title = "Scott-Knott",
--- R/usktest.R
+++ R/usktest.R
@@ -1,20 +1,45 @@
-#' ...
+#' Unbalanced Scott-Knott (USK)
 #'
-#' ...
+#' @description \code{\link{usktest}} is a function used to do the Scott-Knott
+#' cluster analyses (1974) for unbalanced designs proposed at 2017 (CONRADO, Thiago
+#' Vincenzi et al). To learn more, see the \href{http://ref.scielo.org/ws792m}{article}
 #'
-#' @param dataset ...
-#' @param var1 ...
-#' @param var2 ...
-#' @param alpha ...
-#' @param graphic ...
-#' @param ANOVA ...
+#' @param dataset An indication to the database being used.
+#' @param var1 A quantitative variable containing the metric results of the experiment.
+#' @param var2 A factor variable indicating the treatments.
+#' @param alpha Type I error the researcher can accept (the default is 0.05).
+#' @param graphic If TRUE (the default), export a chart ggplotly.
+#' @param ANOVA If TRUE (the default), export an ANOVA table.
 #'
-#' @import purrr dplyr plotly
+#' @import purrr dplyr
+#' @importFrom ggplot2 ggplot aes geom_point scale_y_continuous geom_errorbar theme_bw labs
 #' @importFrom pkgcond suppress_messages
-#' @importFrom ggplot2 ggplot
+#' @importFrom plotly ggplotly layout
 #' @importFrom stats aov qchisq
 #'
+#' @author Felipe Ferreira \email{felipe179971@hotmail.com}
 #'
+#' @examples
+#'
+#' taus=c(4,4,-4,-4,9,-9)
+#' Tratamento<-as.factor(rep(c(paste("trat",seq(1:length(taus)))),3))
+#' erro<-rnorm(3*length(taus),0,1)
+#' y<-2+taus+erro
+#' y[round(runif(1,min=1,max=length(y)),0)]<-NA
+#' dados<-data.frame(y,Tratamento)
+#'
+#' usktest(dados,"y","Tratamento",0.05,graphic=TRUE,ANOVA=TRUE)
+#'
+#' @return This function returns the ANOVA table, a graph and a \code{data.frame} including columns:
+#' \itemize{
+#'  \item "var2": Treatments.
+#'  \item group: Which group the treatment was classified.
+#'  \item mean: Average value of "var1" for each treatment.
+#'  \item min: Lowest value of "var1" for each treatment.
+#'  \item max Higher value of "var1" for each treatment.
+#' }
+#'
+#' @encoding UTF-8
 #' @export
 
 usktest <-
@@ -32,13 +57,16 @@ function(dataset,var1,var2,alpha,graphic,ANOVA){
       Graphic(as.data.frame(RUNscott(dataset,var1,var2,alpha,ANOVA(dataset,var1,var2)[["df.residual"]],summary(ANOVA(dataset,var1,var2))[[1]][["Mean Sq"]][[2]])))
     }
     if(ANOVA==T){
-      Anova<-data.frame(summary(ANOVA(dados,"y","Tratamento"))[[1]])
+      Anova<-data.frame(summary(ANOVA(dataset,"y","Tratamento"))[[1]])
       rownames(Anova)[1]<-var2
       print("##########################ANOVA###########################")
       print(Anova)
       print("#######################Scott-Knott########################")
 
     }
-    return(RUNscott(dataset,var1,var2,alpha,ANOVA(dataset,var1,var2)[["df.residual"]],summary(ANOVA(dataset,var1,var2))[[1]][["Mean Sq"]][[2]])[,-6])
+    Result<-RUNscott(dataset,var1,var2,alpha,ANOVA(dataset,var1,var2)[["df.residual"]],summary(ANOVA(dataset,var1,var2))[[1]][["Mean Sq"]][[2]])[,-6]
+    colnames(Result)[1]<-var2
+    return(Result)
   }
 }
+
